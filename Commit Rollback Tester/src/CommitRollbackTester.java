335775f500d8162eb0a8c37b8595c78a605aca11
McGuire <McGuire@MU-C1-TeacherB.mullen.pvt.k12.co.us>
2018-02-15 11:05:38 -0700

    add statement #2

--- Commit Rollback Tester/src/CommitRollbackTester.java
+++ Commit Rollback Tester/src/CommitRollbackTester.java
@@ -4,5 +4,6 @@ public class CommitRollbackTester
 		public static void main(String[] args)
 			{
 			System.out.println("This is statement #1");
+			System.out.println("This is statement #2");
 			}
 	}
